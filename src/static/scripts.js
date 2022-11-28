3a8695ef215e832f96ab4bcaf20c8e92f37dfffd
Ivan Robles <navitux@disroot.org>
2022-11-28 16:40:07 -0600

    minor fixes, and scripts file addded

---
+++ src/static/scripts.js
@@ -0,0 +1,10 @@
+// particles animation
+new JParticles.Particle('#bg-prt',{
+    maxR: 2,
+    maxSpeed: 0.5,
+    minR: 2,
+    minSpeed: 0.1,
+    num: 0.08,
+    range: 0,
+    proximity:0.3,
+})
